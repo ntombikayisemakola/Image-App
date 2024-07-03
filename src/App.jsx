37bcf302ce43a4a4989037a5d858b37889cb393f
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-07-03 09:53:45 +0200

    Frontend is up but not yet connected

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginForm from './Components/LoginForm/LoginForm'; 
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm />
+      </div>
+  );
+};
+
+export default App;
